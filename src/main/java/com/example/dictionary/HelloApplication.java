0bf730b67536983d0b319f03201d8de35c2d89e5
LamTienDat <lamdat280202@gmail.com>
2021-10-22 14:02:10 +0700

    giaodien

--- src/main/java/com/example/dictionary/HelloApplication.java
+++ src/main/java/com/example/dictionary/HelloApplication.java
@@ -1,5 +1,4 @@
 package com.example.dictionary;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,7 +9,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+          FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         final int width = 700, height = 500;
         Scene scene = new Scene(fxmlLoader.load(), height, width);
         stage.setMinHeight(height);
