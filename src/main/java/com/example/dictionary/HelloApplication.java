5248874bac93a89ed89e2a13c45fff2ae972c416
nana <dongocanh18112002@gmail.com>
2021-10-24 22:11:40 +0700

    na xinh gai

--- src/main/java/com/example/dictionary/HelloApplication.java
+++ src/main/java/com/example/dictionary/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        final int width = 700, height = 500;
+        final int width = 1280, height = 800;
         Scene scene = new Scene(fxmlLoader.load(), height, width);
         stage.setMinHeight(height);
         stage.setMinWidth(width);
