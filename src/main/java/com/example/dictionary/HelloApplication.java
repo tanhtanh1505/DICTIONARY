bae215b2d05321ed3f61c36bbd12a95187a4b2c0
Minh Trung <20021456@vnu.edu.vn>
2021-11-02 00:25:43 +0700

    finish

--- src/main/java/com/example/dictionary/HelloApplication.java
+++ src/main/java/com/example/dictionary/HelloApplication.java
@@ -11,18 +11,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("first-view.fxml"));
-        final int width = 700, height = 500;
-        Scene scene = new Scene(fxmlLoader.load(), height, width);
-
-        stage.setMinHeight(height);
-        stage.setMinWidth(width);
-
-        stage.setMaxHeight(height);
-        stage.setMaxWidth(width);
-
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setResizable(false);
         stage.setTitle("Dictionary");
         stage.setScene(scene);
-
         stage.show();
     }
 
@@ -34,6 +26,7 @@ public class HelloApplication extends Application {
         RankHangMan.Save();
         System.out.println("Stopped!");
     }
+
     public static void main(String[] args) {
         launch();
     }
